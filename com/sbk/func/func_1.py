f511c97d75640fe1d090deb650d17be5748cc416
Serhii Sobolev <sergiy.sobolev@gmail.com>
2018-10-18 02:55:37 +0300

    Refactored jupyter demo

--- com/sbk/func/func_1.py
+++ com/sbk/func/func_1.py
@@ -1,6 +1,6 @@
-def func(t):
+def func_1(t):
     return 5 * (t - 3) ** 4 + 2 * t - 12
 
 
-def calc_gradient(t):
+def calc_gradient_1(t):
     return 20 * (t - 3) ** 3 + 2
--- tests/com/sbk/gradientdescent/test_gradient_descent_single_variable.py
+++ tests/com/sbk/gradientdescent/test_gradient_descent_single_variable.py
@@ -1,6 +1,6 @@
 import unittest
 
-from com.sbk.func.func_1 import calc_gradient, func
+from com.sbk.func.func_1 import calc_gradient_1, func_1
 from com.sbk.gradientdescent.gradient_descent import gradient_descent_single_variable
 
 
@@ -10,16 +10,16 @@ class TestGradientDescentSingleVariable(unittest.TestCase):
         pass
 
     def test_should_find_minimum_with_default_start_approximation(self):
-        x, f = gradient_descent_single_variable(func_calc_gradient=calc_gradient, func=func, learning_rate=0.0001)
+        x, f = gradient_descent_single_variable(func_calc_gradient=calc_gradient_1, func=func_1, learning_rate=0.0001)
         self.assertAlmostEqual(x, 2.53506948945, places=4)
         self.assertAlmostEqual(f, -6.696234472468862, places=4)
 
     def test_should_find_minimum_with_start_approximation_1(self):
-        x, f = gradient_descent_single_variable(func_calc_gradient=calc_gradient, func=func, start_x=1.3)
+        x, f = gradient_descent_single_variable(func_calc_gradient=calc_gradient_1, func=func_1, start_x=1.3)
         self.assertAlmostEqual(x, 2.5358338694353826, places=4)
         self.assertAlmostEqual(f, -6.696234472468862, places=4)
 
     def test_should_find_minimum_with_start_approximation_4(self):
-        x, f = gradient_descent_single_variable(func_calc_gradient=calc_gradient, func=func, start_x=2.7)
+        x, f = gradient_descent_single_variable(func_calc_gradient=calc_gradient_1, func=func_1, start_x=2.7)
         self.assertAlmostEqual(x, 2.5358338694353826, places=4)
         self.assertAlmostEqual(f, -6.696234472468862, places=4)
