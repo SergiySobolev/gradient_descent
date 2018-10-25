e937cb088795c32cde04e531436d3637925f0966
Serhii Sobolev <sergiy.sobolev@gmail.com>
2018-10-25 03:14:18 +0300

    New demo + new tests for dataset

--- tests/com/sbk/gradientdescent/test_batch_gradient_descent.py
+++ tests/com/sbk/gradientdescent/test_batch_gradient_descent.py
@@ -19,3 +19,12 @@ class TestBatchGradientDescent(unittest.TestCase):
         theta0, theta1 = batch_gradient_descent(data)
         self.assertAlmostEqual(theta0, 0.9665143757)
         self.assertAlmostEqual(theta1, 0.47481285077)
+
+    def test_batch_gradient_descent_2(self):
+        uploaded_data = pd.read_csv(
+            'https://raw.githubusercontent.com/SergiySobolev/gradient_descent/master/data/data1.csv',
+            delimiter=',')
+        data = np.asarray(uploaded_data)
+        theta0, theta1 = batch_gradient_descent(data)
+        self.assertAlmostEqual(theta0, 0.871737325073544)
+        self.assertAlmostEqual(theta1, -0.09512333650940695)
